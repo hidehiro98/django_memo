028288dd2e23700add1e1618495b1bad636b1973
Ubuntu <ubuntu@ip-172-31-26-252.ap-northeast-1.compute.internal>
2020-03-25 08:29:06 +0000

    コード4-2

--- django_memo/settings.py
+++ django_memo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x4n%0-980b&$%^$@@au+bq8u@@&+1b3_$ld*$w7%f9*x8#jxec'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'memo',
 ]
 
 MIDDLEWARE = [
