9503d53b2e0db26051db28e1c72ec47f6582cc76
Hidehiro Nagaoka <kuinige@gmail.com>
2020-03-26 05:57:43 +0000

    Added models.py, forms.py and migrated

--- memo/models.py
+++ memo/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
-# Create your models here.
+class Memo(models.Model):
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
