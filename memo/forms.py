886bca721525c901dd2228bcff8c3465892f87d7
Hidehiro Nagaoka <kuinige@gmail.com>
2020-03-26 06:11:51 +0000

    Added views.py

---
+++ memo/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from .models import Memo
+class MemoForm(forms.ModelForm):
+    class Meta:
+        model = Memo
+        fields = ('content',)
--- memo/views.py
+++ memo/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Memo
+from .forms import MemoForm
 
-# Create your views here.
+def show(request): # idから表示するmemoを取得
+    memo_id = request.GET.get('id')
+    memo = Memo.objects.get(pk=memo_id)
+    return HttpResponse("%s" % (memo.content))
+
+def create(request): # GETメソッドの場合フォームを表示、POSTの場合memoを保存
+    if request.method == 'GET':
+        form = MemoForm()
+        return render(request, 'create.html', {'form': form})
+    elif request.method == 'POST':
+        form = MemoForm(request.POST)
+        if form.is_valid():
+            memo = form.save(commit=False)
+            memo.save()
+            return redirect('/memo?id=%s' % memo.pk)
